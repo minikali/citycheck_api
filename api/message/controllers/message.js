3bc29f49d003d774db6682005ebe0ef053611d60
lak_r <remi.lak@epitech.eu>
2020-02-18 16:36:18 +0100

    fixed message.js

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -3,7 +3,7 @@ const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 module.exports = {
   async create(ctx) {
     const body = ctx.request.body;
-    await strapi.services.message.create(ctx.request.body);
+    const entry = await strapi.services.message.create(ctx.request.body);
 
     // send email now if does not have file to upload
     if (!body.has_file) {
