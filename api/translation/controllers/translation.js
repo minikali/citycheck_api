3ff1470f2ae105ea6478e4de295e7cea0f5716ab
lak_r <remi.lak@epitech.eu>
2020-05-01 01:41:34 +0200

    added translation.json route

--- api/translation/controllers/translation.js
+++ api/translation/controllers/translation.js
@@ -5,4 +5,20 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async getTranslation(ctx) {
+    const { lng } = ctx.params;
+    let entities;
+
+    if (ctx.query._q) {
+      entities = await strapi.services.translation.search(ctx.query);
+    } else {
+      entities = await strapi.services.translation.find(ctx.query);
+    }
+    const translations = {};
+    entities.forEach(entity => {
+      translations[entity.key] = entity[lng];
+    });
+    return translations;
+  }
+};
